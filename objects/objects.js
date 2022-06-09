2caed80fc5a3458c30d5f11c5985b0788c86ff07
Aphenphos <MaxwillWinters@gmail.com>
2022-06-09 13:14:55 -0400

    object made test not working

---
+++ objects/objects.js
@@ -0,0 +1,10 @@
+export function makePet() {
+    const pet = {
+        name: 'felix',
+        type: 'cat',
+        age: 6,
+        food: 'tuna',
+        toys: ['feather chaser', 'laser pointer', 'ball of yarn']
+    };
+    return pet;
+}
---
+++ objects/objects.test.js
@@ -0,0 +1,11 @@
+import {
+    makePet
+} from './objects.js';
+
+const test = QUnit.test;
+
+QUnit.module('objects');
+
+test('makes a pet', (expect) => {
+
+});
