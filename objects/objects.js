d2eccbf010cd751367fde0d2fa9924e51edc2f29
Aphenphos <MaxwillWinters@gmail.com>
2022-06-09 13:47:09 -0400

    book problem done

--- objects/objects.js
+++ objects/objects.js
@@ -9,7 +9,16 @@ export function makePet(name, type, age, food, toys) {
     return pet;
 }
 
-export function formatWork(book) {
-
+export function formatWork(data) {
+    const book = {
+        work: data.book.title,
+        writer: `${data.author.first} ${data.author.last}`
+    };
+    return book;
 }
+
+
+
+
+
 
--- objects/objects.test.js
+++ objects/objects.test.js
@@ -1,4 +1,5 @@
 import {
+    formatWork,
     makePet
 } from './objects.js';
 
@@ -12,9 +13,37 @@ test('makes a pet', (expect) => {
         'cat',
         '6',
         'tuna',
-        ['feather chaser', 'laser pointer', 'ball of yarn']\
+        ['feather chaser', 'laser pointer', 'ball of yarn']
     );
     expect.deepEqual(actual, {
+        name: 'felix',
+        type: 'cat',
+        age: '6',
+        food: 'tuna',
+        toys: ['feather chaser', 'laser pointer', 'ball of yarn']
+    });
+
+});
+
+test('makes a book description', (expect) => {
+    const actual = formatWork(
+        { 
+            author: {
+                last: 'wells', 
+                first: 'h.g.'
+            },
+            book: {
+                title: 'war of the worlds', 
+                genre: 'scifi',
+                words: 312000
+            }
+        }
+    );
+    expect.deepEqual(actual, {
+        
+        work: 'war of the worlds', 
+        writer: 'h.g. wells' 
+        
     });
 
 });
